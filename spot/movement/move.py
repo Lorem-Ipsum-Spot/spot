f37c84760ce2534b351643b152b1a17216aa0bcc
Maneren <49210777+Maneren@users.noreply.github.com>
2024-03-15 17:52:29 +0100

    fix movement class

--- spot/movement/move.py
+++ spot/movement/move.py
@@ -1,18 +1,19 @@
+from bosdyn.client.robot_state import RobotStateClient
 from bosdyn.geometry import EulerZXY
-from bosdyn.client.robot_command import RobotCommandBuilder
+from bosdyn.client.robot_command import RobotCommandBuilder, RobotCommandClient
+
 
 class Move:
-    def __init__(self, commandClient) -> None:
-        self.command_client = commandClient
+    state_client: RobotStateClient
+    command_client: RobotCommandClient
 
-    def standUp(self, yaw=0.0, roll=0.0, pitch=0.0):
+    def __init__(self, state_client, command_client) -> None:
+        self.state_client = state_client
+        self.command_client = command_client
+
+    def stand(self, yaw=0.0, roll=0.0, pitch=0.0, height=0.1):
         footprint_R_body = EulerZXY(yaw, roll, pitch)
-        cmd = RobotCommandBuilder.synchro_stand_command(footprint_R_body=footprint_R_body)
+        cmd = RobotCommandBuilder.synchro_stand_command(
+            footprint_R_body=footprint_R_body, body_height=height
+        )
         self.command_client.robot_command(cmd)
-
-        cmd = RobotCommandBuilder.synchro_stand_command(body_height=0.1)
-
-        self.command_client.robot_command(cmd) #used to command dog
-
-
-        #robot.power_off(cut_immediately=False)
