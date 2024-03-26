263fcdd64316812c2693adc04aa437aa8707bb32
vaneceka <vanecek@students.zcu.cz>
2024-03-26 14:27:30 +0100

    nevim

--- spot/__main__.py
+++ spot/__main__.py
@@ -1,14 +1,8 @@
-from spot_sdk import SpotCameraClient
+
 
 if __name__ == "__main__":
     from spot.cli import main
 
     main()
 
-def connect():
-    # Inicializace klienta (předpokládané metody)
-    client = SpotCameraClient(username='user', password='password', robot_ip='192.168.50.3')
-
-    # Připojení k robotu
-    client.connect()
     
