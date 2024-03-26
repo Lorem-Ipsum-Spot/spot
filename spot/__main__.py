7ab29a5b8ab79b035e0ebf87e603b54e81ed21c4
Petr <petr.schopp2@seznam.cz>
2024-03-26 14:27:45 +0100

    rename

--- spot/__main__.py
+++
@@ -1,4 +0,0 @@
-if __name__ == "__main__":
-    from spot.cli import main
-
-    main()
---
+++ spot/vision.py
@@ -0,0 +1,10 @@
+import sys
+import time
+import math
+
+if __name__ == "__main__":
+    from spot.cli import main
+
+    main()
+
+#toto je muj branch, na kterem nekdy budu pracovat (surely)
