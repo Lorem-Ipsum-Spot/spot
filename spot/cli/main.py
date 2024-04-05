71755f85ca5443cde85e76f9709a20266d763d59
Maneren <49210777+Maneren@users.noreply.github.com>
2024-04-05 17:07:55 +0200

    use new version of movement module

--- spot/cli/main.py
+++ spot/cli/main.py
@@ -13,6 +13,7 @@ from spot.cli.curses import run_curses_gui
 from spot.cli.server import run_http_server
 from spot.movement.move import Move
 
+
 def main():
     parser = ArgumentParser()
     bosdyn_util.add_base_arguments(parser)
@@ -49,54 +50,49 @@ def main():
         for client in (RobotStateClient, RobotCommandClient, LeaseClient)
     ]
 
-    # def f():
-    #     import time
-    #
-    #     print("Starting Curses GUI")
-    #     time.sleep(1)
-    #     return run_curses_gui(estop_client, state_client)
-    #
-    # p = Process(target=f, args=("bob",))
-    # p.start()
-    # p.join()
-    #
-    # print("program")
-    
+    def f():
+        import time
+
+        print("Starting Curses GUI")
+        time.sleep(1)
+        return run_curses_gui(estop_client, state_client)
+
+    p = Process(target=f)
+    p.start()
+
     run_http_server()
 
-    print("program")
-    
-    with LeaseKeepAlive(lease_client, must_acquire=True, return_at_exit=True):
-        example_movement_sequence(robot, state_client, command_client)
+    # with LeaseKeepAlive(lease_client, must_acquire=True, return_at_exit=True):
+    #     print("Powering on robot... This may take several seconds.")
+    #     robot.power_on(timeout_sec=20)
+    #     assert robot.is_powered_on(), "Robot power on failed."
+    #     print("Robot powered on.")
+    #
+    #     print("Creating movement controller")
+    #     mover = Move(command_client)
+    #     print("Movement controller ready")
+    #
+    #     example_movement_sequence(mover)
+    #
+    #     print("Powering off...")
+    #     robot.power_off(cut_immediately=False, timeout_sec=20)
+    #     assert not robot.is_powered_on(), "Robot power off failed."
+    #     print("Robot safely powered off.")
+
+    p.join()
 
 
-def example_movement_sequence(robot, state_client, command_client):
-    print("Powering on robot... This may take several seconds.")
-    robot.power_on(timeout_sec=20)
-    assert robot.is_powered_on(), "Robot power on failed."
-    print("Robot powered on.")
-
-    print("Creating movement controller")
-    movement_controller = Move(state_client, command_client)
-    print("Movement controller ready")
-
+def example_movement_sequence(mover: Move):
     print("Commanding robot to stand...")
-    movement_controller.stand(height=0.5)
+    mover.stand()
     print("Robot standing.")
     time.sleep(3)
 
-    print("Twist")
-    movement_controller.stand(yaw=0.4)
+    mover.forward()
     time.sleep(3)
 
-    print("Back")
-    movement_controller.stand(height=0.8)
+    mover.backward()
     time.sleep(3)
-
-    print("Powering off...")
-    robot.power_off(cut_immediately=False, timeout_sec=20)
-    assert not robot.is_powered_on(), "Robot power off failed."
-    print("Robot safely powered off.")
 
 
 def ensure_client(robot, client):
@@ -105,7 +101,7 @@ def ensure_client(robot, client):
     print(f"{client.__name__} initialized")
 
     return result
- 
+
 
 def load_credential_from_file(credentials):
     with open(credentials, "r") as file:
