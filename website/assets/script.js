4dea5e39852536e56ff40f46eab5f8382cd4c862
Minsqui <simonduda12@gmail.com>
2024-04-05 17:53:51 +0200

    adding follow button + fixing switch button styles

--- website/assets/script.js
+++ website/assets/script.js
@@ -1,17 +1,38 @@
 let listening = false;
+let following = false;
 
 function ListeningSwitch(element)
 {
     if (listening == false)
     {
         listening = true;
-        element.style.backgroundColor="green";
+        element.classList.remove("switchButton")
+        element.classList.add("switchButtonActive");
         element.innerHTML="Listening here";
     }
     else
     {
         listening = false;
-        element.style = null;
+        element.classList.remove("switchButtonActive")
+        element.classList.add("switchButton");
         element.innerHTML = "Listening there";
+    }
+}
+
+function FollowingSwitch(element)
+{
+    if (following == false)
+    {
+        following = true;
+        element.classList.remove("switchButton")
+        element.classList.add("switchButtonActive")
+        element.innerHTML = "Following";
+    }
+    else
+    {
+        following = false;
+        element.classList.remove("switchButtonActive")
+        element.classList.add("switchButton")
+        element.innerHTML = "Not following";
     }
 }
