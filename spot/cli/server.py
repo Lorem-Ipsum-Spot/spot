a90d74f27c430fb346ad807a20808d66408d059c
Voky <voky.consultant@gmail.com>
2024-04-18 12:47:01 +0200

    added STOP button functionality, send all info from browser to server and pseudo-handle them

--- spot/cli/server.py
+++ spot/cli/server.py
@@ -11,9 +11,10 @@ def run_http_server():
     HttpServer.add_handle("/cli", test_handler)
     HttpServer.run(host="0.0.0.0", port=4321)
 
+# ------- Handling HTTP requests from client -------
 
-@app.route("/cli/server.py", methods=['POST'])
-def handle_post_request():
+@app.route("/api/movement", methods=['POST'])
+def handle_post_request_movement():
     if request.method == 'POST':
         data = request.get_json()
 
@@ -26,24 +27,53 @@ def handle_post_request():
         if vect[2] != 0:
             print(f"z = {vect[2]}")
 
-        return jsonify({"message" : f"Movement OK {vect}"}), 200
+        return jsonify({"message" : f"Movement vector: {vect}"}), 200
 
     else:
         data = {"message": "Invalid request method from server (Movement)"}
         return jsonify(data), 200
-    
 
-@app.route("/cli/voky.py", methods=['POST'])
-def handle_post_request2():
+
+@app.route("/api/followingStatus", methods=['POST'])
+def handle_post_request_following():
     if request.method == 'POST':
         data = request.get_json()
 
         flw = data.get("follow")
         print(f"following: {flw}")
 
-        return jsonify({"message" : f"Following OK: {flw}"}), 200
+        return jsonify({"message" : f"Following Status: {flw}"}), 200
     
     else:
         data = {"message": "Invalid request method from server (Follow)"}
         return jsonify(data), 200
+
+
+@app.route("/api/listeningStatus", methods=['POST'])
+def handle_post_request_listening():
+    if request.method == 'POST':
+        data = request.get_json()
+
+        lst = data.get("listen")
+        print(f"listening: {lst}")
+
+        return jsonify({"message" : f"Listening Status: {lst}"}), 200
     
+    else:
+        data = {"message": "Invalid request method from server (Listen)"}
+        return jsonify(data), 200
+
+
+@app.route("/api/stop", methods=['POST'])
+def handle_post_request_stop():
+    if request.method == 'POST':
+        data = request.get_json()
+
+        stop = data.get("stop")
+        print(f"Spots operator wants it to STOP immediately!")
+
+        return jsonify({"message" : f"Spots operator wants it to STOP immediately!"}), 200
+    
+    else:
+        data = {"message": "Invalid request method from server (Stop)"}
+        return jsonify(data), 200
--- website/assets/communication.js
+++ website/assets/communication.js
@@ -1,14 +1,16 @@
 
 setInterval(sendAll, 1000);
 
+// every time period send info about buttons push-status in the moment
 async function sendAll(){
     sendMovementVector();
-    sendFollowing();
+    sendFollowingStatus();
+    sendListeningStatus();
 }
 
-
+// send vector [x,y,z] to server - called periodically.
 async function sendMovementVector(){
-    const url = "/cli/server.py";
+    const url = "/api/movement";
 
     const data = {
         dir : directions,
@@ -38,12 +40,76 @@ async function sendMovementVector(){
     });
 }
 
-
-async function sendFollowing(){
-    const url = "/cli/voky.py";
+// send bool following to server - called periodically.
+async function sendFollowingStatus(){
+    const url = "/api/followingStatus";
 
     const data = {
         follow : following,
+    }
+
+    const response = await fetch(url,{
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer',
+        body: JSON.stringify(data),
+    })
+
+    .then(response => response.json())
+    
+    .then(data => {
+        console.log('Response from server:', data);
+    })
+    
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
+}
+
+// send bool listening to server - called periodically.
+async function sendListeningStatus(){
+    const url = "/api/listeningStatus";
+
+    const data = {
+        listen : listening,
+    }
+
+    const response = await fetch(url,{
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer',
+        body: JSON.stringify(data),
+    })
+
+    .then(response => response.json())
+    
+    .then(data => {
+        console.log('Response from server:', data);
+    })
+    
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
+}
+
+// send bool stop to server - only called when stop clicked.
+async function stopSpot(){
+    const url = "/api/stop";
+
+    const data = {
+        stop : true,
     }
 
     const response = await fetch(url,{
