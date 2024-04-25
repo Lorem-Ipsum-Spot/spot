77510d2707d92f6649b8ffcaca44e5284b0d3a93
Voky <voky.consultant@gmail.com>
2024-04-25 13:15:56 +0200

    black formatter - formatted server.py

--- spot/cli/server.py
+++ spot/cli/server.py
@@ -11,14 +11,16 @@ def run_http_server():
     HttpServer.add_handle("/cli", test_handler)
     HttpServer.run(host="0.0.0.0", port=4321)
 
+
 # ------- Handling HTTP requests from client -------
 
-@app.route("/api/movement", methods=['POST'])
+
+@app.route("/api/movement", methods=["POST"])
 def handle_post_request_movement():
-    if request.method != 'POST':
+    if request.method != "POST":
         data = {"message": "Invalid request method from server (Movement)"}
         return jsonify(data), 200
-        
+
     data = request.get_json()
     vect = data.get("dir")
 
@@ -29,47 +31,46 @@ def handle_post_request_movement():
     if vect[2] != 0:
         print(f"z = {vect[2]}")
 
-    return jsonify({"message" : f"Movement vector: {vect}"}), 200
+    return jsonify({"message": f"Movement vector: {vect}"}), 200
 
 
-@app.route("/api/followingStatus", methods=['POST'])
+@app.route("/api/followingStatus", methods=["POST"])
 def handle_post_request_following():
-    if request.method != 'POST':
+    if request.method != "POST":
         data = {"message": "Invalid request method from server (Follow)"}
         return jsonify(data), 200
-        
+
     data = request.get_json()
     flw = data.get("follow")
-    
+
     print(f"following: {flw}")
 
-    return jsonify({"message" : f"Following Status: {flw}"}), 200        
+    return jsonify({"message": f"Following Status: {flw}"}), 200
 
 
-@app.route("/api/listeningStatus", methods=['POST'])
+@app.route("/api/listeningStatus", methods=["POST"])
 def handle_post_request_listening():
-    if request.method != 'POST':        
+    if request.method != "POST":
         data = {"message": "Invalid request method from server (Listen)"}
         return jsonify(data), 200
 
     data = request.get_json()
     lst = data.get("listen")
-    
+
     print(f"listening: {lst}")
 
-    return jsonify({"message" : f"Listening Status: {lst}"}), 200
+    return jsonify({"message": f"Listening Status: {lst}"}), 200
 
 
-@app.route("/api/stop", methods=['POST'])
+@app.route("/api/stop", methods=["POST"])
 def handle_post_request_stop():
-    if request.method != 'POST':
+    if request.method != "POST":
         data = {"message": "Invalid request method from server (Stop)"}
         return jsonify(data), 200
 
     data = request.get_json()
     stop = data.get("stop")
-    
+
     print(f"Spots operator wants it to STOP immediately!")
 
-    return jsonify({"message" : f"Spots operator wants it to STOP immediately!"}), 200
-    
+    return jsonify({"message": f"Spots operator wants it to STOP immediately!"}), 200
