037de54a4b39e31034c95026d6fdde3a00bf1ea2
Maneren <49210777+Maneren@users.noreply.github.com>
2024-04-29 21:45:15 +0200

    remove redundant printing and comments

--- spot/audio/main.py
+++ spot/audio/main.py
@@ -1,21 +1,14 @@
 import speech_recognition as sr
 
-# Initialize the recognizer
 recognizer = sr.Recognizer()
 
 
-# Function to listen to microphone input
 def listen_microphone() -> str | None:
     with sr.Microphone() as source:
-        print("Řekněte něco:")
         recognizer.adjust_for_ambient_noise(source)
         # TODO: look into using `listen_background`
         audio = recognizer.listen(source)
-
     try:
-        command = recognizer.recognize_vosk(audio, language="cs-CZ").lower()
-        print("Řekli jste:", command)
-        return command
+        return recognizer.recognize_vosk(audio, language="cs-CZ").lower()
     except sr.UnknownValueError:
-        print("Nerozumím zvuku")
         return None
