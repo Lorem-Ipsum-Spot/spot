06c4456ab295d837a94687c7011f04ded193a656
vaneceka <160487894+vaneceka@users.noreply.github.com>
2024-05-02 19:20:27 +0200

    pridani funkce pro rozpoznani text do image_recognition.py

--- spot/vision/image_recognition.py
+++ spot/vision/image_recognition.py
@@ -3,6 +3,7 @@ from enum import IntEnum
 
 import cv2
 import numpy as np
+from paddleocr import PaddleOCR, draw_ocr
 
 PATH_TO_MODEL = (
     pathlib.Path(cv2.__file__).parent.absolute() / "data" / "haarcascade_lowerbody.xml"
@@ -72,6 +73,47 @@ def detect_lowerbody(frame: np.ndarray) -> Direction | None:
         return Direction.RIGHT
     else:
         return Direction.CENTER
+
+
+def detect_text(frame):
+    ocr = PaddleOCR(
+        use_angle_cls=True, lang="en"
+    )  # Nastavení jazyka a detekce orientace textu
+    while True:
+        result = ocr.ocr(frame, cls=True)
+        for line in result:
+            if line != None:
+                points = line[0][0]
+                toplx = points[3][0]
+                toply = points[3][1]
+                botdx = points[1][0]
+                botdy = points[1][1]
+                print(points[0])
+                print(points[3])
+                text = line[0][1]
+                if text[0] == "FOLLOW" or text[0] == "follow":
+                    cv2.rectangle(
+                        frame,
+                        (int(toplx), int(toply)),
+                        (int(botdx), int(botdy)),
+                        (255, 245, 0),
+                        2,
+                    )
+            cv2.imshow("Faces", frame)
+        if cv2.waitKey(1) == ord("q"):
+            break
+    cv2.destroyAllWindows()
+
+
+                        (int(toplx), int(toply)),
+                        (int(botdx), int(botdy)),
+                        (255, 245, 0),
+                        2,
+                    )
+            cv2.imshow("Faces", frame)
+        if cv2.waitKey(1) == ord("q"):
+            break
+    cv2.destroyAllWindows()
 
 
 # TODO: implement
